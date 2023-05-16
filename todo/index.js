edd31b489c284f67023d544a3cdb01100d7e3ae2
codegrann <omondihills02@gmail.com>
2023-05-16 05:29:49 +0300

    trigered addButton to add new task when clicked

--- todo/index.js
+++ todo/index.js
@@ -1,2 +1,18 @@
 let addButton = document.querySelector("#addBtn");
-addButton.style.fontSize="5rem";
+addButton.addEventListener("click", addTask);
+
+function addTask(){
+    let input=document.querySelector("#addText");
+    let tasks=document.querySelector("#tasks");
+    if(input.value.length == 0){
+        alert("Please enter title of a task to continue.")
+    }
+    else{
+        tasks.innerHTML=`
+        <div class="taskEach">
+          <input type="text" class="task taskText">
+          <button class="remove button">&times;</button>
+        </div> 
+        `
+    }
+}
