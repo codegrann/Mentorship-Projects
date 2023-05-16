88bdacd21ad369bda5acd7a359d03248cff1096f
codegrann <omondihills02@gmail.com>
2023-05-16 05:35:42 +0300

    gave margin bottom to newly added tasks

--- todo/index.js
+++ todo/index.js
@@ -1,16 +1,16 @@
 let addButton = document.querySelector("#addBtn");
 addButton.addEventListener("click", addTask);
 
-function addTask(){
-    let input=document.querySelector("#addText");
-    let tasks=document.querySelector("#tasks");
-    if(input.value.length == 0){
+function addTask() {
+    let input = document.querySelector("#addText");
+    let tasks = document.querySelector("#tasks");
+    if (input.value.length == 0) {
         alert("Please enter title of a task to continue.")
     }
-    else{
-        tasks.innerHTML=`
+    else {
+        tasks.innerHTML += `
         <div class="taskEach">
-          <input type="text" class="task taskText">
+          <input type="text" class="task taskText" value="${input.value}" readonly>
           <button class="remove button">&times;</button>
         </div> 
         `
